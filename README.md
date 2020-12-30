e8345ae104a789bfa3d11d482b7dff7688a566a4
CXS <2327085154@qq.com>
2020-12-30 17:08:16 +0800

    🎉 fristCommit

---
+++ README.md
@@ -0,0 +1,6 @@
+# contribution_calendar
+
+一款基于Flutter开发的显示github贡献日历的APP。
+
+## 预览
+![01](img\01.jpg)
---
+++ android/app/src/main/kotlin/top/pythong/contribution_calendar/MainActivity.kt
@@ -0,0 +1,6 @@
+package top.pythong.contribution_calendar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
